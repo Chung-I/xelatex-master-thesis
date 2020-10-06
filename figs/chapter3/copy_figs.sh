9a465b85c56c50a3d4b54d2260d4e8591ba899e0
Chung-I <leon129506@gmail.com>
2020-10-06 09:38:04 +0800

    update

--- figs/chapter3/copy_figs.sh
+++
@@ -1,3 +0,0 @@
-cp ~/Works/acl-2020/allennlp-maml-parsing/results/adapters/dir_size_las_ft_colortbsize_multi-adapter-lr3e-4.pdf dir_size_las_ft_multi.pdf
-cp ~/Works/acl-2020/allennlp-maml-parsing/results/adapters/dir_size_las_ft_colortbsize_reptile-adapter-lr3e-4-noinherit-to-multi-adapter-lr3e-4.pdf dir_size_las_ft_reptile-to-multi.pdf
-cp ~/Works/acl-2020/allennlp-maml-parsing/results/adapters/dir_size_las_ft_colortbsize_fomaml-adapter-lr3e-4-noinherit-to-multi-adapter-lr3e-4.pdf dir_size_las_ft_fomaml-to-multi.pdf
